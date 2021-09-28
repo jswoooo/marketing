c5914e36a9a3fa1a9a1ce8d191b07c8100a318d0
jswoooo <hak02110721@gmail.com>
2021-09-28 12:47:19 +0900

    real

--- src/main/java/com/grp04/togosvc/marketing/external/OrderService.java
+++ src/main/java/com/grp04/togosvc/marketing/external/OrderService.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-//@FeignClient(name="Order", url="http://order:8080")
-@FeignClient(name="order", url="http://localhost:8082")
+@FeignClient(name="Order", url="http://order:8080")
+//@FeignClient(name="order", url="http://localhost:8082")
 public interface OrderService {
     @RequestMapping(method= RequestMethod.GET, path="/orders/search/removeByPlanId?planId={id}")
     public void returnToGo(@PathVariable long id);
